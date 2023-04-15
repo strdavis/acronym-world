d8ef8cdea9c4c5fecc27ce105616f515bc317429
strdavis <9757975+strdavis@users.noreply.github.com>
2023-04-15 00:08:35 -0700

    Create empty single-page app.

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders landing screen title', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Welcome!/i);
+  expect(linkElement).toBeInTheDocument();
+});
